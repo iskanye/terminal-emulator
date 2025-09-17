569c0d34e15be8a135d175cd565f6eba5c24bc55
исканье <iskanderbikta@yandex.ru>
2025-09-17 14:00:27 +0300

    tail and change start script

--- programs/programs.go
+++ programs/programs.go
@@ -1,6 +1,11 @@
 // Пакет со всеми доступными встроенными командами
 package programs
 
+import (
+	"fmt"
+	"strings"
+)
+
 // Исполняемая функции программы:
 // in - входной канал в который поступают аргументы программы;
 // out - выходной канал в который поступает результат работы программы;
@@ -9,7 +14,24 @@ type Program func(in chan string, out chan interface{}, err chan error)
 
 // Встроенные команды
 var Programs = map[string]Program{
-	"ls": Ls,
-	"cd": Cd,
-	"du": Du,
+	"ls":   Ls,
+	"cd":   Cd,
+	"du":   Du,
+	"tail": Tail,
+}
+
+// Функция извлечения параметров
+func ExtractArgs(args []string) (map[string]string, error) {
+	if len(args)%2 != 0 {
+		return nil, fmt.Errorf("wrong number of args")
+	}
+
+	result := make(map[string]string)
+	for i := 0; i < len(args); i++ {
+		key := strings.Trim(args[i], "-")
+		i++
+		result[key] = args[i]
+	}
+
+	return result, nil
 }
---
+++ programs/tail.go
@@ -0,0 +1,71 @@
+package programs
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"terminal-emulator/vfs"
+)
+
+func Tail(in chan string, out chan interface{}, stderr chan error) {
+	n := 10
+	args := make([]string, 0)
+	for i := range in {
+		args = append(args, i)
+	}
+
+	if len(args) == 0 {
+		stderr <- fmt.Errorf("no args")
+		return
+	} else if len(args) > 1 && len(args) < 4 {
+		argv, err := ExtractArgs(args[:len(args)-1])
+		if err != nil {
+			stderr <- err
+			return
+		}
+
+		if val, ok := argv["n"]; ok {
+			n, err = strconv.Atoi(val)
+			if err != nil {
+				stderr <- err
+				return
+			}
+		} else if val, ok := argv["lines"]; ok {
+			n, err = strconv.Atoi(val)
+			if err != nil {
+				stderr <- err
+				return
+			}
+		} else {
+			stderr <- fmt.Errorf("wrong arg: %s", args[0])
+			return
+		}
+	} else if len(args) != 1 {
+		stderr <- fmt.Errorf("too many args")
+		return
+	}
+
+	file, err := vfs.FileExplorer.GetFile(args[len(args)-1])
+	if err != nil {
+		stderr <- err
+		return
+	}
+
+	content, err := file.Read()
+	if err != nil {
+		stderr <- err
+		return
+	}
+
+	lines := strings.Split(content, "\n")
+
+	if len(lines) <= n {
+		out <- content
+	} else {
+		for i := n; i > 0; i-- {
+			out <- lines[len(lines)-i-1]
+		}
+	}
+
+	stderr <- nil
+}
