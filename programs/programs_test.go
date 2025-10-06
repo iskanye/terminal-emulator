aa11882f0d0b6043d0d7f6b8e41bc1aaa65c768b
iskanye <iskanyeban@gmail.com>
2025-10-06 11:22:00 +0300

    feat: ls test

---
+++ programs/programs_test.go
@@ -0,0 +1,75 @@
+package programs
+
+import (
+	"fmt"
+	"sync"
+	"terminal-emulator/vfs"
+	"testing"
+)
+
+var testVFS string = `<node name="" dir="true" modified="2025-09-17T15:24:55.4406365+03:00">
+	<node name="home" dir="true" modified="2025-09-17T15:24:55.4406365+03:00">
+		<node name="user" dir="true" modified="2025-09-17T15:24:55.4406365+03:00">
+		<node name="document.txt" dir="false" modified="2025-09-20T13:24:15.4254299+03:00">
+			<content>doc1&#xA;doc2&#xA;doc3&#xA;doc4&#xA;doc5&#xA;doc6&#xA;doc7&#xA;doc8&#xA;doc9&#xA;doc10&#xA;doc11</content>
+		</node>
+		<node name="image.jpg" dir="false" modified="2025-09-17T15:24:55.4406365+03:00">
+			<content>image12345</content>
+		</node>
+		<node name="empty" dir="true" modified="2025-09-17T15:24:55.4406365+03:00"></node>
+		<node name="empty.txt" dir="false" modified="2025-09-18T13:32:11.0540706+03:00"></node>
+		<node name="ff.txt" dir="false" modified="2025-09-20T13:24:27.524069+03:00"></node>
+		</node>
+		<node name="touch" dir="false" modified="2025-09-19T12:52:13.9736937+03:00"></node>
+	</node>
+	<node name="etc" dir="true" modified="2025-09-17T15:24:55.4406365+03:00">
+		<node name="config.conf" dir="false" modified="2025-09-17T15:24:55.4406365+03:00">
+		<content>configuration</content>
+		</node>
+	</node>
+</node>`
+
+func setupVFS() {
+	fs := vfs.LoadFromString([]byte(testVFS))
+	vfs.SetupExplorer(fs)
+}
+
+func TestLs(t *testing.T) {
+	out := make([]string, 0, 2)
+	correct := []string{"home", "etc"}
+	ls := Program(Ls)
+	wg := &sync.WaitGroup{}
+
+	setupVFS()
+	InitChannels()
+	wg.Add(3)
+
+	go func() {
+		defer wg.Done()
+		WriteToStdin([]string{})
+	}()
+
+	go func() {
+		defer wg.Done()
+		ls()
+	}()
+
+	go func() {
+		defer wg.Done()
+		if err := <-stderr; err != nil {
+			t.Errorf("unexpected error: %s", err)
+		}
+	}()
+
+	for i := range stdout {
+		out = append(out, fmt.Sprint(i))
+	}
+
+	wg.Wait()
+
+	for i := range out {
+		if out[i] != correct[i] {
+			t.Errorf("\nexpected: %s\ngot: %s", correct[i], out[i])
+		}
+	}
+}
--- vfs/serialization.go
+++ vfs/serialization.go
@@ -5,6 +5,20 @@ import (
 	"os"
 )
 
+// Загрузить корневую ноду из строки
+func LoadFromString(data []byte) *Node {
+	root := &Node{}
+	if err := xml.Unmarshal([]byte(data), root); err != nil {
+		root := NewRoot()
+		return root
+	}
+
+	// Создаём в каждой ноде указатель на родителя
+	root.restoreParent(nil)
+
+	return root
+}
+
 // Загрузить корневую ноду с xml файла
 func LoadFromXML(file string) *Node {
 	xmlData, err := os.ReadFile(file)
@@ -13,17 +27,7 @@ func LoadFromXML(file string) *Node {
 		return NewRoot()
 	}
 
-	root := &Node{}
-	if err := xml.Unmarshal([]byte(xmlData), root); err != nil {
-		root := NewRoot()
-		root.SaveToXML(file)
-		return NewRoot()
-	}
-
-	// Создаём в каждой ноде указатель на родителя
-	root.restoreParent(nil)
-
-	return root
+	return LoadFromString(xmlData)
 }
 
 // Сохранить ветку в xml файл
